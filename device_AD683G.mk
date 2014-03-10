84b0b75bfd68b497a030600e6846d134f2e8d539
alexforsale <alexforsale@yahoo.com>
2014-03-11 00:39:14 +0700

    adds graphics flags and product packages, also audio product packages

--- device_AD683G.mk
+++ device_AD683G.mk
@@ -53,6 +53,38 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     gps.default
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.primary.default \
+    audio.primary.msm7627a \
+    audio.policy.default \
+    audio.policy.msm7627a
+
+# Graphics 
+PRODUCT_PACKAGES += \
+    copybit.msm7627a \
+    gralloc.default \
+    gralloc.msm7627a \
+    hwcomposer.msm7627a \
+    libgenlock \
+    lights.msm7627a
+
+# mm-audio
+PRODUCT_PACKAGES += \
+    sw-adec-omxaac-test \
+    sw-adec-omxamr-test \
+    sw-adec-omxamrwb-test \
+    sw-adec-omxmp3-test \
+    libOmxAacEnc \
+    mm-aenc-omxaac-test \
+    libOmxAmrEnc \
+    mm-aenc-omxamr-test \
+    libOmxEvrcEnc \
+    mm-aenc-omxevrc-test \
+    libOmxQcelp \
+    mm-aenc-omxqcelp13-test
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
